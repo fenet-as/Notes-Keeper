5443b0bdc623d993ff4825dbe9c439ce260095d1
fenet-as <fenetasmamaw48@gmail.com>
2025-08-27 17:44:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ import { createNoteElement, addNote, deleteNote, updateNote } from "./notes.js";
 const addBtn = document.getElementById("add-note-btn");
 const appContainer = document.getElementById("app");
 
-// Load existing notes
+
 loadNotes().forEach(note => {
   const noteEl = createNoteElement(note.id, note.content, deleteNote, updateNote);
   appContainer.insertBefore(noteEl, addBtn);
 });
 
-// Add new note on click
+
 addBtn.addEventListener("click", () => {
   addNote(appContainer, addBtn);
 });
